fff48e7cfb74e01fbee8fd0b577f71a476779e10
Patrick Rodrigues <rodpa715@outlook.com>
2019-02-26 15:55:36 +0100

    rewrote the whole script and renamed html id's

--- script.js
+++ script.js
@@ -1,91 +1,88 @@
-let decision = ["yes", "y", "YES", "maybe","M", "MAYBE", "meh", "no", "n", "NO"]
-let yes = "0";
-let no = "0";
-let textFieldHtml;
-let percentageNumbers = []
-let clicks = 0;
-let percent = 0;
-let avg = 0;
+let currentRandomNumber = 0;
+let allRandomNumbers = [];
+let averageRandomNumbers = 0;
+/** HTML INTERACTION VARIABLES**/
 
-/** Span **/
-let display = document.getElementById("decisionDisplay")
-let percentage = document.getElementById("percentage")
-let clicksHtml = document.getElementById("clicks")
-let avgHtml = document.getElementById("avg")
-let finalHtml = document.getElementById("final")
-
-function main(){
-    yes = 0;
-    no = 0;
-    for(i=0; i<100; i++){
-        decider();
-        decisionStore();
-        textChanger();
-        percentageCalculator();
-    }
-    pushToArray();
-    clicks++;
-    clicksHtml.textContent = clicks;
-    averageCalculator();
-    avgHtml.textContent = Math.ceil(avg);
-    makeFinalDecision();
+let clicksHtml = document.getElementById("clicks");
+let displayInputHtml = document.getElementById("displayInput");
+let percentageHtml = document.getElementById("percentage");
+let avgHtml = document.getElementById("avg");
+let finalHtml = document.getElementById("final");
+/** Creates an array from 0-100 **/
+let probability = [];
+for (let i = 0; i <= 100; i++ ){
+    probability.push(i)
 }
-/** Randomly decides wheter yes or no **/
-function decider() {
-	choice = decision[Math.floor(Math.random() * decision.length)];
-    return choice
+/** Chooses a random number from the array above */
+function getRandomNumber() {
+	randomNumber = probability[Math.floor(Math.random() * probability.length)];
+    return currentRandomNumber = randomNumber
 }
-
-/** Counts the yes or no's**/
-function decisionStore(){
-    if(choice === "yes" || choice === "y" || choice === "YES"){
-        yes=+ ((3 + 3)*2)*3
-        //console.log(yes)
-    } else if (choice === "no" || choice === "n" || choice === "NO"){
-        no=+ ((3 + 3)*2)*3
-        //console.log(no)
-    } else  if (choice === "maybe"){
-        no=+ (2 + 2)*2
-    } else if (choice === "M"){
-        yes=+ (2 + 2)*2
-    } else if (choice === "MAYBE"){
-        yes=+ (2 + 2)*2
-        no=+ (2 + 2)*2
-    } else if (choice  === "meh"){
-        yes=+ (2 + 2)*2
-        no=+ (2 + 2)*2
-    }
+/** Displays the values on the webpage**/
+function displayOnWebpage(){
+    inputHtml = document.getElementById("input").value;
+    displayInputHtml.textContent = inputHtml;
+    percentageHtml.textContent = currentRandomNumber;
+    avgHtml.textContent = averageRandomNumbers;
+    clicksHtml.textContent = allRandomNumbers.length;
 }
-
-function textChanger(){
-    //fetches text 
-    textFieldHtml = document.getElementById("decision").value;
-    //changes text content
-    display.textContent = textFieldHtml;
-}
-
-function percentageCalculator(){
-    percent = Math.round((yes/100)*100);
-    percentage.textContent = percent;
-}
-
+/** Pushes the current random number into an array**/
 function pushToArray(){
-    percentageNumbers.push(percent)
-    console.log("array: " + percentageNumbers)
+    allRandomNumbers.push(currentRandomNumber);
 }
-
-function averageCalculator(){
+/** Calculates the average of the array**/
+function calculateAverage(){
     let total = 0;
-    for (i = 0; i < percentageNumbers.length; i++){
-        total += percentageNumbers[i]
+    for(i = 0; i < allRandomNumbers.length; i++){
+        total = total + allRandomNumbers[i]
     }
-    avg = total / percentageNumbers.length
+    averageRandomNumbers = Math.round(total / allRandomNumbers.length);
 }
-
-function makeFinalDecision(){
-    if (avg < 50){
-        finalHtml.textContent = "GO FOR IT!!"
-    } else if(avg > 50){
-        finalHtml.textContent = "STOP DON'T DO IT"
+/** onclick function executes code once**/
+function decide(){
+    getRandomNumber();
+    pushToArray();
+    calculateAverage();
+    displayOnWebpage();
+    displayMessage();
+    console.log("Current Random Number: " + currentRandomNumber);
+    console.log("Random Numbers Array: " + allRandomNumbers)
+    console.log("Average of Array: " + averageRandomNumbers)
+    console.log("Times Decided: " + allRandomNumbers.length)
+}
+/** onclick function executes code 100x**/
+function bulkDecide(){
+    let i
+    for (i = 0; i < 100; i++){
+        decide();
+    }
+    i = 0;
+    console.log("Bulk times decided: " + allRandomNumbers.length)
+}
+/** Resets all values to 0 **/
+function resetValues(){
+    currentRandomNumber = 0;
+    allRandomNumbers = [];
+    averageRandomNumbers = 0;
+    finalHtml.textContent = "";
+    displayOnWebpage();
+    console.log("Reseted Current Random Number: " + currentRandomNumber);
+    console.log("Reseted Random Numbers Array: " + allRandomNumbers)
+    console.log("Reseted Average of Array: " + averageRandomNumbers)
+    console.log("Reseted Times Decided: " + allRandomNumbers.length)
+}
+console.log("array: " + probability);
+/** Displays Messages based on average **/
+function displayMessage(){
+    if (averageRandomNumbers === 50){
+        finalHtml.textContent = "50/50 chance... Take Risks or Don't..."
+    } else if (averageRandomNumbers === 0 || averageRandomNumbers <= 2 ){
+        finalHtml.textContent = "Chances are very very slim, stop!"
+    }else if (averageRandomNumbers === 100 || averageRandomNumbers >= 98 ){
+        finalHtml.textContent = "Chances are very high! Dive in!"
+    }else if (averageRandomNumbers > 50){
+        finalHtml.textContent = "What are you waiting for? Go for it!"
+    }else if (averageRandomNumbers < 50){
+        finalHtml.textContent = "No, but you really shouldn't ask a computer for advice..."
     }
 }
